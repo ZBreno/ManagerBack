0a21cf63b5edb992411c0956a053c258fe484936
Jose_Neto <ze.neto429@gmail.com>
2023-07-24 15:07:36 -0300

    Add user to deppartment

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -17,7 +17,7 @@ class DepartmentViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.validated_data['']
+        serializer.validated_data['user'] = request.user
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
